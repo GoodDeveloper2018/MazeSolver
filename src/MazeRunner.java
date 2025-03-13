3199a5bbcdd2e7c2ac4dcd842a4e81056a233436
BT_2S14_19 <arshpurohit@gmail.com>
2025-03-13 11:04:30 -0400

    Just some initial stuff with lots of errors

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -1,5 +1,4 @@
 public class MazeRunner {
     public static void main(String[] args) {
-        
     }
 }
--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,9 +1,41 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MazeSolver {
+    int[] navgeableRows = {-1, 0, 0, 1};
+    int[] navegableColumns = {-1, 0, 0, 1};
+    public static String[][] maze = getMaze("src/input.txt") ;
+    public static int[][] visited;
+    public ArrayList<List<Integer>> solutionMaze = new ArrayList<List<Integer>>();
+    String block = "#";
+    String move = ".";
+    int coordX;
+    int coordY;
+
+    public int[] getCoordinates() {
+        int[] coordinates = {coordX, coordX};
+        return coordinates;
+    }
+
+    public int[] getNavgeableRows() {
+        return navgeableRows;
+    }
+
+    public void setNavgeableRows(int[] navgeableRows) {
+        this.navgeableRows = navgeableRows;
+    }
+
+    public int[] getNavegableColumns() {
+        return navegableColumns;
+    }
+
+    public void setNavegableColumns(int[] navegableColumns) {
+        this.navegableColumns = navegableColumns;
+    }
+
     public static String[][] getMaze(String fileName) {
         File f = new File("input.txt");
         Scanner s = null;
@@ -22,17 +54,24 @@ public class MazeSolver {
         int rows = fileData.size();
         int cols = fileData.get(0).length();
 
-        String[][] maze = new String[rows][cols];
+        String[][] Maze = new String[rows][cols];
 
         for (int i = 0; i < fileData.size(); i++) {
             String d = fileData.get(i);
             for (int j = 0; j < d.length(); j++) {
-                maze[i][j] = d.charAt(j) + "";
+                Maze[i][j] = d.charAt(j) + "";
             }
         }
-        return maze;
+        return Maze;
     }
 
+    public int[][] solveMaze() {
+        int initialX = 0;
+        int initialY = 0;
 
-
+        while(maze[initialX++][initialY++].equals(block.getCoordiate()) != true || maze[initialX++][initialY].equals(block.getCoordiate()) != true) {
+            solveMaze();
+        }
+        return solutionMaze;
+    }
 }
