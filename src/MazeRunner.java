68e97eae5092ae8ff2b011813daf06888421228b
Arsh <80232792+GoodDeveloper2018@users.noreply.github.com>
2025-03-14 10:43:08 -0400

    Update MazeRunner.java
    Fixed File finding issue and added check method for solution and "YAY!"

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -1,9 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MazeRunner {
-    public static void main(String[] args) {
-        MazeSolver solver = new MazeSolver();
+    public static void main(String[] args) throws FileNotFoundException {
+        String dasSolution = new Scanner(new File("src/DasSolution.txt")).useDelimiter("\\Z").next();
+        String mySolution = new Scanner(new File("src/MySolution.txt")).useDelimiter("\\Z").next();
+        if (mySolution.equals(dasSolution)) {
+            System.out.println("YAY!");
+        }
+        MazeSolve solver = new MazeSolve();
         ArrayList<List<Integer>> path = solver.solveMaze();
 
         if (path.isEmpty()) {
@@ -14,7 +22,7 @@ public class MazeRunner {
             List<Integer> coord = path.get(i);
             System.out.print("(" + coord.get(0) + ", " + coord.get(1) + ")");
             if (i < path.size() - 1) {
-                System.out.print(" --> ");
+                System.out.print(" ---> ");
             }
         }
         System.out.println();
